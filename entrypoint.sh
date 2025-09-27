dde24a5a6e539d32572c3a0a1e4c8e294fabb218
Ekaansh <ekaansh.sahni@gmail.com>
2025-09-27 23:57:35 +0530

    added gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ echo "Database is up!"
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 300

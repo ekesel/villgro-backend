8f8b380c05546023cbf5b7266b360f80eaa55a5b
Ekaansh <ekaansh.sahni@gmail.com>
2025-10-08 00:09:31 +0530

    admin changes

--- assessments/admin.py
+++ assessments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Assessment, Answer
+
 
 # Register your models here.
+
+admin.site.register(Assessment)
+admin.site.register(Answer)
--- questionnaires/admin.py
+++ questionnaires/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Section, Question, QuestionDimension, AnswerOption, BranchingCondition
 
 # Register your models here.
+
+admin.site.register(Section)
+admin.site.register(Question)
+admin.site.register(QuestionDimension)
+admin.site.register(AnswerOption)
+admin.site.register(BranchingCondition)

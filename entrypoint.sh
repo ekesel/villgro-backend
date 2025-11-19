faa58f9d38ba10e20e0ce7aba71039e1f73a94a7
Ekaansh <ekaansh.sahni@gmail.com>
2025-11-20 02:43:20 +0530

    updaed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ until nc -z "${DB_HOST}" "${DB_PORT}" >/dev/null 2>&1; do
 done
 echo "Database is up!"
 
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 300
